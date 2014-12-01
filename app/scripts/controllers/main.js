4eec29729f78954044eafe1870ee0deec4405dbe
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-30 22:26:09 -0800

    switch between views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,31 +10,23 @@
 angular.module('newAngApp')
   .controller('MainCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
 
-    // Views
-    $scope.views = ['main', 'instas','twitter'];
-    $scope.activeView = $scope.views[0];
-
-    // Term entered by user
-    $scope.term = '';
-
-    // URLs to fetch content
-    var baseURL = 'http://mediadashapi.herokuapp.com/';
+    // Templates
+    $scope.templates = {
+      available: ['main', 'instas', 'tweet'],
+      active: 'views/main.html',
+      activeIndex: 0
+    };
+    console.log($scope.templates);
     
-    // HTTP requests
-    $http.get($scope.tweetUrl).success(function(data) {
-      console.log(data);
-      $scope.tweet = data[0];
-      $scope.tweets = data;
-    });
-    $http.get($scope.instaUrl).success(function(data) {
-      console.log(data);
-      $scope.instas = data;
-    });
+    // Defaults
+    $scope.term = '';
+    var baseURL = 'http://mediadashapi.herokuapp.com/';
 
     $scope.getTweets = function() {
       $http.get($scope.tweetUrl).success(function(data) {
         console.log(data);
         $scope.tweets = data;
+        $scope.tweet = data[0];
       });
     };
 
@@ -45,18 +37,31 @@ angular.module('newAngApp')
       });
     };
 
-    $scope.submit = function() {
-      $scope.term = $scope.term.replace(/\#/, '');
+    $scope.submit = function(term) {
+      $scope.term = term.replace(/\#/, '');
+      console.log($scope.term);
       $scope.tweetUrl = baseURL + 'twitter?term=' + $scope.term;
       $scope.instaUrl = baseURL + 'insta?term=' + $scope.term;
       $scope.getInstas();
       $scope.getTweets();
-      $scope.changeViews(1);
+      $scope.changeActiveTemplate(1);
     };
 
-    $scope.changeViews = function(index) {
-      $scope.activeView = $scope.views[index];
-      $location.path($scope.activeView);
+    $scope.changeActiveTemplate = function(index) {
+      $scope.templates.active = 'views/' + $scope.templates.available[index] + '.html';
+      $scope.templates.activeIndex = index;
+    };
+
+    $scope.previousTemplate = function(){
+      if( $scope.templates.activeIndex > 0 ) {
+        $scope.changeActiveTemplate( $scope.templates.activeIndex - 1) ;
+      }
+    };
+
+    $scope.nextTemplate = function(){
+      if( $scope.templates.activeIndex < $scope.templates.available.length - 1 ) {
+        $scope.changeActiveTemplate( $scope.templates.activeIndex + 1 ) ;
+      }
     };
 
     $scope.parseDate = function(date){
