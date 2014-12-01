da5fa85e48cc29b02a1ed37fb62acd4898e2210b
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-30 16:38:26 -0800

    search and switch views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,14 @@
  * Controller of the newAngApp
  */
 angular.module('newAngApp')
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+
+    // Views
+    $scope.views = ['main', 'instas','twitter'];
+    $scope.activeView = $scope.views[0];
 
     // Term entered by user
-    $scope.term = 'football';
+    $scope.term = '';
 
     // URLs to fetch content
     var baseURL = 'http://mediadashapi.herokuapp.com/';
@@ -20,17 +24,40 @@ angular.module('newAngApp')
     
     // HTTP requests
     $http.get($scope.tweetUrl).success(function(data) {
-          console.log(data);
-          $scope.tweet = data[0];
-          $scope.tweets = data;
+      console.log(data);
+      $scope.tweet = data[0];
+      $scope.tweets = data;
     });
     $http.get($scope.instaUrl).success(function(data) {
-          console.log(data);
-          $scope.instas = data;
+      console.log(data);
+      $scope.instas = data;
     });
 
+    $scope.getTweets = function() {
+      $http.get($scope.tweetUrl).success(function(data) {
+            console.log(data);
+            $scope.tweets = data;
+      });
+    };
 
+    $scope.getInstas = function() {
+       $http.get($scope.instaUrl).success(function(data) {
+             console.log(data);
+             $scope.instas = data;
+       });
+    };
 
+    $scope.submit = function() {
+      $scope.term = $scope.term.replace(/\#/, '');
+      $scope.getInstas();
+      $scope.getTweets();
+      $scope.changeViews(1);
+    };
+
+    $scope.changeViews = function(index) {
+      $scope.activeView = $scope.views[index];
+      $location.path($scope.activeView);
+    };
 
     $scope.parseDate = function(date){
       return Date.parse(date);
