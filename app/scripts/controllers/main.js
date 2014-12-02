35b24da5946ca4618a6f28680f1c4e44023abcca
julian.gallegos@gmail.com <julian.gallegos@gmail.com>
2014-12-02 10:27:12 -0800

    removed duplicate lines

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -132,14 +132,7 @@ angular.module('newAngApp')
       }, 8000);
     }
 
-    //this function only cycle through insta and tweets and then stops
-    //// I should create a way to have this cycle back and forth between tweets and instas and other views 
-    //// we create
-    var cycleThroughViews = function(){
-      $interval(function(){
-        $scope.nextTemplate();
-      }, 8000);
-    }
+
 
 
 
