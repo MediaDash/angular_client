fb3c78a5e53cae57d90c336dda4eb70eb50fd87f
Andrew Archer <andrew.m.archer@gmail.com>
2014-12-04 17:04:20 -0800

    switch to 9393 for preso

--- app/scripts/services/sockets.js
+++ app/scripts/services/sockets.js
@@ -11,8 +11,8 @@ angular.module('mySocket', [])
   .factory('socket', function ($rootScope) {
   // var socket = io.connect('http://mediadashapi.herokuapp.com:5000');
   console.log('nuh uh');
-  // var socket = io.connect('http://localhost:9393');
-    var socket = io.connect('http://salty-journey-1875.herokuapp.com:80/');
+    var socket = io.connect('http://localhost:9393');
+    // var socket = io.connect('http://salty-journey-1875.herokuapp.com:80/');
     return {
       on: function (eventName, callback) {
           socket.on(eventName, function () {
